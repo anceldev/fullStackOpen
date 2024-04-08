03554ac7e0ebfe2811aa2f03f810569fc1d30bd8
Ancel Guarayo <aguarayo@gmail.com>
2024-04-08 17:20:18 +0200

    part3 e2

--- part3/phonebookapp/index.js
+++ part3/phonebookapp/index.js
@@ -33,7 +33,11 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
-
+app.get('/info', (request, response) => {
+  const numPersons = persons.count
+  console.log(request.date)
+  response.send(`<p>Phonebook has info for ${numPersons} people<p><p>${new Date()}</p>`)
+})
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
